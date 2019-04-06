1ff59d6572c9ffa51967e0c58bf220a60cb19645
Dave Block <dave@northcreek.ca>
2019-04-06 11:15:49 -0600

    change zoom level again

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -108,7 +108,7 @@
               html: `<span style="${markerStyle(5)}" />`
             });
 
-            var mymap = L.map('mapid').setView([{{$lastResponse->lat}}, {{$lastResponse->long}}], 11);
+            var mymap = L.map('mapid').setView([{{$lastResponse->lat}}, {{$lastResponse->long}}], 10);
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                 maxZoom: 18,
