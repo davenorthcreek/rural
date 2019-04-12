c8af4dc92958c21ec3bac76ee78b7a16c5d5ec48
Dave Block <dave@northcreek.ca>
2019-04-12 13:37:47 -0600

    new sidebar links

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -31,10 +31,12 @@
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
+            <li class="header">Links</li>
             <!-- Optionally, you can add icons to the links -->
             <li><a href="/survey"><i class='fa fa-question'></i> <span>The Survey</span></a></li>
             <li><a href="/about"><i class='fa fa-link'></i> <span>About the Co-op</span></a></li>
+            <li><a href="https://newsletter.firstworldrural.ca"><i class='fa fa-newspaper-o'></i> <span>Newsletter</span></a></li>
+            <li><a href="https://forum.firstworldrural.ca"><i class='fa fa-comments-o'></i> <span>Forum</span></a></li>
             @if (! Auth::guest())
             <li><a href="/all"><i class='fa fa-users'></i> <span>All Survey Responses</span></a></li>
             @endif
